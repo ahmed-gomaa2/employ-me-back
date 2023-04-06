73ca3deaf89e3f61e9c070a35632b3e34b751f54
AhmedGomaaaAli <engahmedgomaa1997@gmail.com>
2023-04-06 16:11:41 +0200

    removing the keys file

--- keys/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongURI: 'mongodb+srv://ahmed:ahmed@cluster0.heix1.mongodb.net/?retryWrites=true&w=majority',
-    jwtSecret: "mysecrettoken",
-    GithubClientID: '255cf94434c431c4ae17',
-    GithubClientSecret: 'b04dec08007d97ef9d235397d9016e8238e91555'
-}
